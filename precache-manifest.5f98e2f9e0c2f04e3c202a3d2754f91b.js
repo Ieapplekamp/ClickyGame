e73c11a1d0a06ca746b1c891d08348c922a806e5
Ian Applekamp <ieapplekamp@gmail.com>
2019-12-22 20:48:36 -0500

    Updates

---
+++ precache-manifest.5f98e2f9e0c2f04e3c202a3d2754f91b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f204f895434c34fa9926b1dfc181f3bd",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "22397f53712c5ea8c5ca",
+    "url": "/ClickyGame/static/js/2.fdc3b523.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ClickyGame/static/js/2.fdc3b523.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4df936eb1e7bd8e5d5f0",
+    "url": "/ClickyGame/static/js/main.113ff9bf.chunk.js"
+  },
+  {
+    "revision": "b989ed8cd4092df789c5",
+    "url": "/ClickyGame/static/js/runtime-main.6dbadd8e.js"
+  }
+]);
