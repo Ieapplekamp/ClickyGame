5db062620d715c0cd4378584a95d2ceff9731275
Ian Applekamp <ieapplekamp@gmail.com>
2019-12-27 16:07:21 -0500

    FINALLY got the state to change and render null

--- clickygame/src/components/Intro/Intro.js
+++ clickygame/src/components/Intro/Intro.js
@@ -3,36 +3,40 @@ import React, { Component } from "react";
 import "../Intro/Intro.css"
 
 class Intro extends Component {
-    state = {
-        isDismissed: false
+
+    constructor() {
+        super();
+        this.state = {
+          show: true,
+        };
     }
 
-    // dismissHandler = () => {
-    //     this.setState({
-    //         isDismissed: true
-    //     })
-    // }
+    dismissable = () => {
 
-    operation() {
-        this.setState({
-            isDismissed: true
-        })
-    }
+        if (this.state.show === true) {
+          this.setState({ show: false });
+        } else {
+          this.setState({ show: true });
+        }
+
+    };
 
     render() {
-        return (
-            // {
-            //     (this.state.isDismissed) ?  : null;
-            // }
-            <section className="section-intro">
-                <img className="intro-pic" onClick={()=>this.operation()} src={require('../images/dawn.png')} alt="dawn pic"></img>
+        console.log("this was triggered")
+        return  this.state.show ? (
+            
+            <section className={`section-intro`}>
+                
+                <img className="intro-pic" onClick={this.dismissable} src={require('../images/dawn.png')} alt="dawn pic"></img>
                 <p>
                 Click on an pokemon to earn points, but don't click on any more than once!
                 <br/><br/><br/>
                 Talk to Dawn to get started!
                 </p>
-           </section>
-        )
+                
+            </section>
+               
+        ) : null 
     }
 }
 
