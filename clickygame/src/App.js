e8a25578ed2c26ad7a4ac17a18fff02f86070410
Ian Applekamp <ieapplekamp@gmail.com>
2019-12-27 17:04:47 -0500

    scaled and deleted extra code

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,70 +1,60 @@
 import React from 'react';
-// import { render } from 'react-dom';
-// import logo from './logo.svg';
-// import './App.css';
 import Matches from "./components/matchmatch.json"
-
 import Navbar from "./components/Navbar/Navbar";
 import Intro from "./components/Intro/Intro";
 import Pics from "./components/Pics/Pics";
 import shuffle from "./components/Shuffle"
 
-var chosenArray = [];
+let chosenArray = [];
 
 class App extends React.Component {
 
-  state = {
-    Matches,
-    currentScore: 0,
-    highScore: 0
-  }
+	state = {
+		Matches,
+    	currentScore: 0,
+    	highScore: 0
+  	}
 
-  // gameLoop handles onclick events for images
-  gameLoop = (event) => {
-    event.preventDefault();
-    // Grabs the unique alt tag of the image clicked on
-    let chosen = event.target.getAttribute('alt');
+	gameLoop = (event) => {
+    	event.preventDefault();
+    
+    	let chosen = event.target.getAttribute('alt');
 
-    // Checks if image has been saved in chosenArray
-    if (chosenArray.includes(chosen)){
-      // Resets score to 0 if already saved in chosenArray
-      this.setState({ currentScore: 0 });
-      // Empties chosen array to reset game
-      chosenArray =[];
-    } else {
-      // If not in chosenArray increments currentScore by 1
-      this.setState({ currentScore: this.state.currentScore + 1 });
-      // Checks the topScore and updates if currentScore is higher than top score
-      if (this.state.currentScore >= this.state.highScore) {
-          this.setState({ highScore: this.state.highScore + 1 });
-      };
-      // Pushes the selected array item to chosenArray
-      chosenArray.push(chosen);
-    };
-
-    // Shuffles the matches array
-    this.setState({ Matches: shuffle(this.state.Matches)});        
-  };
-  
-  render() {
-    return (
-      <div>
-          <Navbar
-            currentScore={this.state.currentScore}
-            highScore= {this.state.highScore}
-          />
-          <Intro 
-            
-          />
-          <Pics
-            Matches={this.state.Matches}
-            gameLoop = {this.gameLoop}
-          />
-      </div>
+    	if (chosenArray.includes(chosen)) {
       
-    );
-  }
+    		this.setState({ currentScore: 0 });
+    		chosenArray = [];
+      
+    	} else {
+      
+    		this.setState({ currentScore: this.state.currentScore + 1 });
 
+    		if (this.state.currentScore >= this.state.highScore) {
+        		this.setState({ highScore: this.state.highScore + 1 });
+    		};
+      
+    		chosenArray.push(chosen);
+    	};
+
+		this.setState({ Matches: shuffle(this.state.Matches)});        
+	};
+  
+	render() {
+		return (
+			<div>
+				<Navbar
+					currentScore={this.state.currentScore}
+					highScore= {this.state.highScore}
+				/>
+				<Intro/>
+				<Pics
+					Matches={this.state.Matches}
+					gameLoop = {this.gameLoop}
+				/>
+			</div>
+      
+    	);
+	}
 }
 
 
--- clickygame/src/components/Navbar/Navbar.js
+++ clickygame/src/components/Navbar/Navbar.js
@@ -1,13 +1,11 @@
 import React from "react";
 import "../Navbar/Navbar.css"
-// import "../images/pokeball.png"
 
 function Navbar(props) {
     return (
         <header>
             <img className="nav-img" src={require('../images/pokeball.png')} alt="pokeball"/>
             <nav className="navbar nav-p">
-            
                 <span className="nav-p">Catch'Em All!</span>
                 <span className="scores">Caught: {props.currentScore} | High Score: {props.highScore}</span>
             </nav>
